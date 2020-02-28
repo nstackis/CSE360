766579d89c6c27e652496b67714ce4473a194bfc
nstackis <nstackis@asu.edu>
2020-02-27 19:32:33 -0700

    These are the changes incorparated in steps 2 and 3

--- CSE 360/src/assign2/SimpleList.java
+++ CSE 360/src/assign2/SimpleList.java
@@ -23,13 +23,18 @@ public class SimpleList
 		list = new int[10];
 	}
 	public void add(int value) // This adds an integer to the array and shifts the remaining values to the right
-	{
-		if(count<10)
+	{                          // The array size is increased by 50% if the array is full
+		if(count<list.length)
 		{
 			count++;
 		}
-		int[] listCopy = Arrays.copyOf(list, 10);
-		for(int loopCounter = 1; loopCounter < count; loopCounter++)
+		else
+		{
+			list = Arrays.copyOf(list, (int)(list.length+.5*list.length));
+			count++;
+		}
+		int[] listCopy = Arrays.copyOf(list, list.length);
+		for(int loopCounter = 1; loopCounter < list.length; loopCounter++)
 		{
 			list[loopCounter] = listCopy[loopCounter - 1];
 		}
@@ -37,7 +42,7 @@ public class SimpleList
 	}
 	public void remove(int value) // This looks for an integer and then removes it if it is in the list
 	{                         // The remaining values are then shifted to the left.
-		int listIndex = -1;
+		int listIndex = -1;   // The array size is decreased by 25% if the array is missing more than 25% of values
 		for(int loopCounter = 0; loopCounter < list.length; loopCounter++)
 		{
 			if(value == list[loopCounter])
@@ -47,12 +52,17 @@ public class SimpleList
 		}
 		if(listIndex > -1)
 		{
-			count--;
-			for(int loopCounter = listIndex; loopCounter < list.length - 2; loopCounter++)
+			int[] tempList = Arrays.copyOf(list, list.length);
+			for(int loopCounter = listIndex; loopCounter < list.length-1; loopCounter++)
 			{
-				list[loopCounter] = list[loopCounter + 1];
+				list[loopCounter] = tempList[loopCounter + 1];
 			}
 			list[list.length -1] = 0;
+			count--;
+		}
+		if(count<(int)(list.length*0.75)&&(int)(list.length*.75)>1)
+		{
+			list = Arrays.copyOf(list, (int)(list.length*.75));
 		}
 	}
 	public int count() // This returns the integer count which is the current number of entries in the list
@@ -62,6 +72,10 @@ public class SimpleList
 	public String toString() // This returns a string that contains all of the values in the list seperated by spaces
 	{
 		String stringList = "";
+		if(count==0)
+		{
+			return stringList;
+		}
 		for(int loopCounter = 0;loopCounter < list.length; loopCounter++)
 		{
 			stringList = stringList +list[loopCounter];
@@ -83,5 +97,45 @@ public class SimpleList
 			}
 		}
 		return listIndex;
+	}
+	public void append(int value) //adds a value to the end of the array and increases the array size
+	{                             // and apend the value to the end if the array was full
+		if(count<list.length)
+		{
+			list[count] = value;
+			count++;
+		}
+		else
+		{
+			list = Arrays.copyOf(list, (int)(list.length*1.5));
+			list[count] = value;
+			count++;
+		}
+	}
+	public int first() //returns the first index of the array if it exists
+	{
+		if(count>0)
+		{
+			return list[0];
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	public int last() // returns the last index of the array if it exists
+	{
+		if(count>0)
+		{
+			return list[count-1];
+		}
+		else
+		{
+			return -1;
+		}
+	}
+	public int size() //returns the size of the array
+	{
+		return list.length;
 	}
 }
